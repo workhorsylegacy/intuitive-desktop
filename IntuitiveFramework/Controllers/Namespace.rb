d385663441dd56dccdf31f47f9e1a13b7ba2c48a
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-01-07 00:08:27 -0800

    Removed the old system communication controller.

--- IntuitiveFramework/Controllers/Namespace.rb
+++ IntuitiveFramework/Controllers/Namespace.rb
@@ -9,5 +9,4 @@ path = File.dirname(File.expand_path(__FILE__))
 'DocumentController',
 'RevisionedFileSystemController',
 'SearchController',
-'UserController',
-'SystemCommunicationController'].each { |file_name| require "#{path}/#{file_name}" }
+'UserController'].each { |file_name| require "#{path}/#{file_name}" }
